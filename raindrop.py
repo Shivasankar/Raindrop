051b34bddb0e97c2e5ce0187e08b77eb775cd63d
Shivasankar <sshiva657@gmail.com>
2014-03-20 00:02:14 +0530

    Refactor created lib, server and test dir Json object is used and able to send the value used request type in raindrop.py

--- raindrop.py
+++ raindrop.py
@@ -7,15 +7,17 @@ def publish(channel, msg):
     port = 8777
     host = socket.gethostname()
     sock.connect((host, port))
-    sock.send(json.dumps({channel: msg}, separators=(',', ':'), sort_keys=True))
+    request_type = 'p'
+    sock.send(json.dumps(({channel: msg}, request_type), separators=(',', ':'), sort_keys=True))
 
 
 def subscribe(channel, fn):
+    request_type = 's'
     sock = socket.socket()
     port = 8777
     host = socket.gethostname()
     sock.connect((host,port))
     while True:
-        sock.send(channel)
+        sock.send(json.dumps({channel, request_type}, separators=(',', ':')))
         msg = sock.recv(1024)
         fn(msg)
